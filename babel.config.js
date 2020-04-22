8949a6f5e6b6533ee2f782f2124f7888b9f35a17
ty <towyuan@outlook.com>
2020-04-22 10:28:45 +0800

    Update

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-    presets: [
-        [
-            '@babel/preset-env',
-            {
-                loose: true,
-                modules: false,
-                targets: {
-                    chrome: 28,
-                    safari: 6,
-                    firefox: 28,
-                    opera: 32,
-                    ie: 11,
-                },
-            },
-        ],
-        '@babel/preset-react',
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        loose: true,
+        modules: false,
+        targets: {
+          chrome: 28,
+          safari: 6,
+          firefox: 28,
+          opera: 32,
+          ie: 11,
+        },
+      },
     ],
-    plugins: [
-        '@babel/plugin-proposal-object-rest-spread',
-        'babel-plugin-transform-es2015-modules-simple-commonjs',
-    ],
+    '@babel/preset-react',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-object-rest-spread',
+    'babel-plugin-transform-es2015-modules-simple-commonjs',
+  ],
 };
